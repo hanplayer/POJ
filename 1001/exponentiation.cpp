c9aeddafd2fadd777effa3dd3cbcfae9673a8d23
hanplayer <hhanfengzhe1@163.com>
2017-07-25 22:39:34 +0800

    完成scanner class 但还未测试

--- 1001/exponentiation.cpp
+++ 1001/exponentiation.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
-//#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 10
+#define VALUE_LENGTH 6
+#define POWER_START_POS_1 7 //大值 幂的开始位置
+#define POWER_START_POS_2 8 //小值 幂的开始位置
+#define POW_LONG_LEN 2 //幂最长的长度
+#define POW_SHORT_LEN 1 //幂最短的长度
 typedef struct _element
 {
-	double value;
+	char value[6];
 	short power;//幂
 }Element;
 
@@ -14,21 +21,67 @@ public:
 	Scanner(){};
 	~Scanner(){};
 	int Scan(void);
-private:
-	std::string rowEnter;//输入的行内容
+	int AppendElem(Elememt &input);//添加一个空的输入
+	int SetValue(const char * input);
+	int SetPower(const char * input);
+
 	vector<Element> recvContent;//输入的内容
 };
+
+
+
 int Scanner::Scan()
 {
 	while(1)
 	{
-		cin>>rowEnter;
-		
+		char recv[MAXLINE];//输入的行内容
+		fgets(recv,MAXLINE,stdin);
+		if(recv[0]=='\n' || recv[0] == ' ')
+		{
+			return -1;
+		}
+		else
+		{
+			Element elem;
+			SetValue(recv,elem);
+			SetPower(recv,elem);
+			AppendElem(elem);
+		}
 	}
 }
 
-int main()
+int Scanner::SetValue(const char *input,Element &output)
 {
-	short  record[125];//记录数字，计算出最大的内容长度为125
+	memcpy(input,output.value,VALUE_LENGTH);
+	return 0;
+}
+//题中述 幂的位置可能从第8位或第9位开始
+//所以要做判断，看是从哪一位开始的
+int Scanner::SetPower(const char *input,Element &output)
+{
+	short result;
+	//幂只有一位
+	if(input[POWER_START_POS_1]!=' ')
+	{
+		result = (input[POWER_START_POS_1] - '0')*10 + (input[POWER_START_POS_2] - '0')*1;
+		
+	}
+	else
+	{
+		result = input[POWER_START_POS_2] - '0';
+	}
+	output.power = result;
+	return 0;
+}
+int Scanner::AppendElem(Element& elem)
+{
+	recvContent.push_back(elem);
+	return 0;
+}
+
+
+int main(void)
+{
 	
+	return 0;
 }
