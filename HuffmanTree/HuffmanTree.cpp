8c436480811618a3632f8da7d95080010e9985c5
hanplayer <hanfengzhe1@163.com>
2017-10-31 23:10:15 +0800

    添加HuffmanTreeNode类的声明和HuffmanTree类的声明

--- HuffmanTree/HuffmanTree.cpp
+++ HuffmanTree/HuffmanTree.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+template <class T>
+class HuffmanTreeNode
+{
+friend class HuffmanTree<T>; 
+private:
+	T info;
+	HuffmanTreeNode<T> *parent;//父节点
+	HuffmanTreeNode<T> *left;//左子节点
+	HuffmanTreeNode<T> *right;//右子节点
+public:
+	HuffmanTreeNode(const T&ele);
+	HuffmanTreeNode(const T&ele,HuffmanTreeNode<T> *left,HuffmanTreeNode<T> *right);//添加了左右子树
+};
+
+template <class T>
+class HuffmanTree
+{
+private:
+	HuffmanTreeNode<T> *root;
+	//将两个根为ht1 和 ht2为根的合并成一颗以parent
+	void MergeTree(HuffmanTreeNode<T> &ht1,HuffmanTreeNode<T> &ht2,HuffmanTreeNode<T> *parent);
+public:
+	HuffmanTree(T weight[],int n);
+	virtual ~HuffmanTree(){DeleteTree(root);};
+};
