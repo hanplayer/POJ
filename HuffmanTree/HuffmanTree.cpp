b484fbce334c9d025ed7322afdec955bec079e0d
admin <admin@colasoft.com.cn>
2017-11-03 10:47:20 -0400

    modified:   HuffmanTree.cpp 	modified:   MinHeap.hpp

--- HuffmanTree/HuffmanTree.cpp
+++ HuffmanTree/HuffmanTree.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "MinHeap.hpp"
+
+template <class T>
+class HuffmanTree;
+
 template <class T>
 class HuffmanTreeNode
 {
 friend class HuffmanTree<T>; 
-private:
+public:
+//private:
 	T info;
 	HuffmanTreeNode<T> *parent;//父节点
 	HuffmanTreeNode<T> *left;//左子节点
@@ -16,28 +21,16 @@ public:
 	HuffmanTreeNode(const T&ele);
 	HuffmanTreeNode(const T&ele,HuffmanTreeNode<T> *left,HuffmanTreeNode<T> *right);//添加了左右子树
 	*/
-	bool operator < (const T&next);
+	bool operator < (const HuffmanTreeNode<T> &next);
+	bool operator ==(const HuffmanTreeNode<T> &next);
 };
 
 
-template <class>
+template <class T>
 HuffmanTreeNode<T>::HuffmanTreeNode():parent(NULL),left(NULL),right(NULL),self(NULL){};
-/*
-template <class T>
-HuffmanTreeNode<T>::HuffmanTreeNode(const T&ele):info(ele),parent(NULL),left(NULL),right(NULL){};
-
 
 template <class T>
-HuffmanTreeNode<T>::HuffmanTreeNode(const T&ele,HuffmanTreeNode<T> *left,HuffmanTreeNode<T> *right)
-{
-	info = ele;
-	parent = NULL;
-	this->left = left;
-	this->right = right;
-}
-*/
-template <class T>
-bool HuffmanTreeNode<T>::operator < (const T&next)
+bool HuffmanTreeNode<T>::operator < (const HuffmanTreeNode<T> &next)
 {
 	if( this->info < next.info )
 	{
@@ -46,12 +39,21 @@ bool HuffmanTreeNode<T>::operator < (const T&next)
 	return false;
 }
 
-
+template <class T>
+bool HuffmanTreeNode<T>::operator == (const HuffmanTreeNode<T> &next)
+{
+	if( this->info == next.info )
+	{
+		return true;
+	}
+	return false;
+}
 
 template <class T>
 class HuffmanTree
 {
-private:
+public:
+//private:
 	HuffmanTreeNode<T> *root;
 	//将两个根为ht1 和 ht2为根的合并成一颗以parent
 	void MergeTree(HuffmanTreeNode<T> &ht1,HuffmanTreeNode<T> &ht2,HuffmanTreeNode<T> *parent);
@@ -83,17 +85,27 @@ HuffmanTree<T>::HuffmanTree(T weight[],int n)
 
 		min1 = heap.RemoveMin();
 		min2 = heap.RemoveMin();
-		MergeTree(min1,min2,parent);
+		MergeTree(min1,min2,par);
 
+		std::cout<<"min1:"<<min1.info<<std::endl;
+		std::cout<<"min2:"<<min2.info<<std::endl;
 		heap.Insert(*par);
 		root = par;
 	}
 }
 
 template <class T>
-void HuffmanTree<T>::MergeTree(HuffmanTreeNode<T> &ht1.,HuffmanTreeNode<T> &ht2,HuffmanTreeNode<T> *parent)
+void HuffmanTree<T>::MergeTree(HuffmanTreeNode<T> &ht1,HuffmanTreeNode<T> &ht2,HuffmanTreeNode<T> *parent)
 {
 	parent->info = ht1.info + ht2.info;
 	parent->left = ht1.self;
 	parent->right = ht2.self;
+}
+
+int main(void)
+{
+	int a[10] = {9,11,5,7,6,3,6,20,29,1};
+	HuffmanTree<int> tree(a,10);
+
+	return 0;
 }
--- HuffmanTree/MinHeap.hpp
+++ HuffmanTree/MinHeap.hpp
@@ -1,5 +1,8 @@
 #include <cmath>// log2f
 #include <algorithm>//std::swap
+#include <iostream>
+#include <stdio.h>//printf()
+#include <memory.h>//memset()
 
 //定义最小堆，堆使用一维数组来存储
 template <class T>
@@ -12,7 +15,7 @@ private:
 	void BuildHeap(void);//建堆
 public:
 	MinHeap(const int n);//n为最大元素数目
-	virtual ~MinHeap(){delete []heapArray};
+	virtual ~MinHeap(){delete []heapArray;};
 	bool isLeaf(int pos)const;
 	int leftchild(int pos)const;//返回左孩子的位置
 	int rightchild(int pos)const;//返回
@@ -34,10 +37,9 @@ void MinHeap<T>::BuildHeap()
 }
 
 template <class T>
-MinHeap<T>::MinHeap(const int n)
+MinHeap<T>::MinHeap(const int n):MaxSize(n),CurrentSize(0)
 {
 	heapArray = new T[n];
-	MaxSize = n;
 }
 
 template <class T>
@@ -125,7 +127,7 @@ T MinHeap<T>::RemoveMin()
 template <class T>
 void MinHeap<T>::SiftUp(int pos)
 {
-	if(pos = 0)
+	if(pos == 0)
 	{
 		return;
 	}
@@ -141,21 +143,81 @@ void MinHeap<T>::SiftUp(int pos)
 template <class T>
 void MinHeap<T>::SiftDown(int pos)
 {
-	if( isLeaf(int pos) )
+	if( (pos*2+1)>(CurrentSize - 1) )
 	{
 		return ;
 	}
 	
 	int left_child_pos = leftchild(pos);//左子节点的位置
 	int right_child_pos = rightchild(pos);//右子节点的位置
-	if( heapArray[left_child_pos] < heapArray[pos] )
+
+	if( heapArray[left_child_pos] < heapArray[pos] && heapArray[pos] < heapArray[right_child_pos] )//左孩子最小
 	{
 		std::swap(heapArray[left_child_pos],heapArray[pos]);
 		SiftDown(left_child_pos);
 	}
-	else if( heapArray[right_child_pos] < heapArray[pos] )
+	else if( heapArray[right_child_pos] < heapArray[pos] && heapArray[pos] < heapArray[left_child_pos] )//右孩子最小
 	{
 		std::swap(heapArray[right_child_pos],heapArray[pos]);
-		SiftUp(right_child_pos);
+		SiftDown(right_child_pos);
+	}
+	else if(  heapArray[left_child_pos] < heapArray[pos] && heapArray[right_child_pos] < heapArray[pos] )//当前节点比两个孩子都大
+	{
+		if( heapArray[left_child_pos] < heapArray[right_child_pos] )//右节点大于左
+		{
+			std::swap(heapArray[left_child_pos],heapArray[pos]);
+			SiftDown(left_child_pos);
+		}
+		else if( heapArray[right_child_pos] < heapArray[left_child_pos] )//左大于右
+		{
+			std::swap(heapArray[right_child_pos],heapArray[pos]);
+			SiftDown(right_child_pos);
+		}
+		else//左等于右
+		{
+			std::swap(heapArray[left_child_pos],heapArray[pos]);
+			SiftDown(left_child_pos);
+		}
+	}
+	else if( heapArray[left_child_pos] < heapArray[right_child_pos] &&  heapArray[right_child_pos] == heapArray[pos] )
+	{
+		std::swap(heapArray[left_child_pos],heapArray[pos]);
+		SiftDown(left_child_pos);		
+	}
+	else if( heapArray[right_child_pos] < heapArray[left_child_pos] && heapArray[left_child_pos] == heapArray[pos] )
+	{
+		std::swap(heapArray[right_child_pos],heapArray[pos]);
+		SiftDown(right_child_pos);
 	}
 }
+/*
+//just test
+int main(void)
+{
+	MinHeap<int> heap(10);
+	int a[10] = {9,11,5,7,6,3,6,20,29,1};
+	for(int i = 0 ; i < 20;i++)
+	{
+		heap.Insert(a[i]);
+	}
+	for(int j = 0;j < 10;j++)
+	{
+		printf("%d\n",heap.heapArray[j]);
+	}
+
+
+	for(int m = 1 ; m <= 10; m++)
+	{
+		std::cout<<"=================="<<m<<"=================="<<std::endl;
+		heap.RemoveMin();
+		for(int j = 0;j < 10 - m;j++)
+		{
+			printf("%d\n",heap.heapArray[j]);
+		}		
+	}
+
+
+
+	return 0;
+}
+*/
